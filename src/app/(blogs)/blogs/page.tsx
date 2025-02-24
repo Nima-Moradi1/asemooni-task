44173029a86ae12600b5ba3397e423f3f79bbc00
nima moradi <mnima8100@gmail.com>
2025-02-24 16:05:24 +0330

    "page" changed to BlogsPage

--- src/app/(blogs)/blogs/page.tsx
+++ src/app/(blogs)/blogs/page.tsx
@@ -2,7 +2,7 @@ import Blogs from "@/components/Blogs";
 import { getBlogsApi } from "@/services/blogsServices";
 
 //since asemooni asked me to fetch blogs SSR, we will async fetch blogs here and pass it as props
-export default async function Home({searchParams}:{searchParams : Promise<{ [key: string]: string | string[] | undefined }>}) {
+export default async function BlogsPage({searchParams}:{searchParams : Promise<{ [key: string]: string | string[] | undefined }>}) {
   
   const search = await searchParams
   const page = search?.page ? parseInt(search.page as string, 10) : undefined;
