5052786f060a4bc4b2240d65537cdbec0a1b0ec0
nima moradi <mnima8100@gmail.com>
2025-02-24 15:57:42 +0330

    form validation

---
+++ src/validation/schemas.ts
@@ -0,0 +1,19 @@
+import * as yup from 'yup' ;
+
+export const SignupSchema = yup.object({
+    firstName: yup.string().required('نام الزامی است'),
+    lastName: yup.string().required('نام خانوادگی الزامی است'),
+    email: yup.string().email('ایمیل نامعتبر است').required('ایمیل الزامی است'),
+    password: yup
+    .string()
+    .min(8, 'پسورد باید حداقل ۸ کاراکتر باشد')
+    .matches(/[A-Za-z]/, 'پسورد باید حداقل یک حرف داشته باشد')
+    .matches(/\d/, 'پسورد باید حداقل یک عدد داشته باشد')
+    .required('پسورد الزامی است'),    phoneNumber: yup.string().matches(/^[0-9]{11}$/, 'شماره همراه نامعتبر است').required('شماره همراه الزامی است'),
+    image : yup.string().required('عکس کاور پست الزامی است')
+})
+
+export const LoginSchema = yup.object({
+    email: yup.string().email('ایمیل نامعتبر است').required('ایمیل الزامی است'),
+    password: yup.string().required('پسورد الزامی است'),
+})
