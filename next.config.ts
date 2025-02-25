1e230e586bae90a3b3f474f13e3e0d73ccf15828
nima moradi <mnima8100@gmail.com>
2025-02-25 13:00:54 +0330

    added domains and hostname

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images : {
+    domains : [
+      'testback.asemooni.org'
+    ] ,
     remotePatterns : [
       {
         protocol : "https",
-        hostname : 'testback.asemooni.org'
+        hostname : 'testback.asemooni.org',
+        pathname: "/uploads/images/**",
       }
     ]
   }
