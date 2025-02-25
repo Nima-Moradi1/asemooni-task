84278b820a63c6d450d2ab01d2ac40f66ee4a4e4
nima moradi <mnima8100@gmail.com>
2025-02-25 16:50:47 +0330

    changed schema to handle a 3-step signup

--- src/validation/schemas.ts
+++ src/validation/schemas.ts
@@ -1,6 +1,12 @@
 import * as yup from 'yup' ;
 
-export const SignupSchema = yup.object({
+
+
+const step1Schema = yup.object({
+    phoneNumber: yup.string().matches(/^[0-9]{11}$/, 'شماره همراه نامعتبر است').required('شماره همراه الزامی است'),
+});
+  
+  const step2Schema = yup.object({
     firstName: yup.string().required('نام الزامی است'),
     lastName: yup.string().required('نام خانوادگی الزامی است'),
     email: yup.string().email('ایمیل نامعتبر است').required('ایمیل الزامی است'),
@@ -9,9 +15,25 @@ export const SignupSchema = yup.object({
     .min(8, 'پسورد باید حداقل ۸ کاراکتر باشد')
     .matches(/[A-Za-z]/, 'پسورد باید حداقل یک حرف داشته باشد')
     .matches(/\d/, 'پسورد باید حداقل یک عدد داشته باشد')
-    .required('پسورد الزامی است'),    phoneNumber: yup.string().matches(/^[0-9]{11}$/, 'شماره همراه نامعتبر است').required('شماره همراه الزامی است'),
-    image : yup.string().required('تصویر الزامی است').default('https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png')
-})
+    .required('پسورد الزامی است'),    
+     });
+  
+  const step3Schema = yup.object({
+    image : yup.string().required('تصویر الزامی است')
+  });
+  
+  export const SignupSchema = (step: number) => {
+    switch (step) {
+      case 1:
+        return step1Schema;
+      case 2:
+        return step2Schema;
+      case 3:
+        return step3Schema;
+      default:
+        return yup.object({});
+    }
+  };
 
 export const LoginSchema = yup.object({
     email: yup.string().email('ایمیل نامعتبر است').required('ایمیل الزامی است'),
