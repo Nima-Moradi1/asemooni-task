fbe42af2526dec23aed533084bf7627c3a55b9b5
nima moradi <mnima8100@gmail.com>
2025-02-25 13:01:18 +0330

    user profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,10 +5,24 @@ import Image from 'next/image'
 
 const UserProfilePage = () => {
   const user = useUserStore((state) => state.user)
-  const userImage = `${process.env.NEXT_PUBLIC_BASE_URL}${user?.image}`
+  const userImage = `${process.env.NEXT_PUBLIC_BASE_URL}${user?.image}` || ''
   return (
     <div>
-      <Image src={userImage || ''} alt='profileImage' width={250} height={350}/>
+      <h1>
+        <span>{user?.firstName} {" "}</span>
+        <span>
+        عزیز خوش آمدید ❤️
+        </span>
+      </h1>
+        <h1 className='h1 mt-5'>اطلاعات حساب کاربری شما</h1>
+      <div className='flex flex-col gap-5 items-center justify-center p-3 border rounded-xl *:border-b'>
+        <Image src={userImage || ''} alt='profileImage' width={170} height={150} className='rounded-full border'/>
+        <p>نام: {user?.firstName}</p>
+        <p>نام خانوادگی: {user?.lastName}</p>
+        <p>نام کاربری: {user?.email}</p>
+        <p>ایمیل: {user?.email}</p>
+        <p>شماره تماس: {user?.phoneNumber}</p>
+      </div>
     </div>
   )
 }
